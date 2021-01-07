c633b95c6322ecd43c582ef15843f04a44c70dd1
Vukovic Emilie <emilievukovic79@gmail.com>
2021-01-07 21:15:46 +0100

    Add modal closure of the form

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const modalCross = document.querySelector(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -20,4 +21,26 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+// Close Modal Event and form
+modalCross.addEventListener("click", function() {
+    modalbg.style.display = "none"; });
 
+/* ! 2 méthodes : laquelle mieux ??? 
+** Demander à Thomas laquelle est la plus correcte
+
+*Close Modal Event and Form
+modalCross.addEventListener("click", function() {
+    modalbg.style.display = "none";
+    }));
+
+                OU
+
+*Close Modal Event
+modalCross.addEventListener("click", closeModal);
+
+*Close Modal Form
+function closeModal() {
+    modalbg.style.display = "none";
+}
+
+*/
