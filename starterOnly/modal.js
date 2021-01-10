05af461c490180532f77b35a64872ee16c7ed295
Vukovic Emilie <emilievukovic79@gmail.com>
2021-01-10 15:09:19 +0100

    add favicon. Homepage desktop version (not completed)
    responsive and mobile not done yet.

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,46 +1,27 @@
 function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
+  const x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
 }
 
 // DOM Elements
 const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
+// const modalBtn = document.querySelector(".modal-btn");
+const modalBtn = document.querySelector(".btn-signup");
 const formData = document.querySelectorAll(".formData");
 const modalCross = document.querySelector(".close");
 
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
-// launch modal form
-function launchModal() {
-  modalbg.style.display = "block";
-}
+// launch modal event and form
+modalBtn.addEventListener("click", function() {
+    modalbg.style.display = "block";
+});
 
-// Close Modal Event and form
-modalCross.addEventListener("click", function() {
-    modalbg.style.display = "none"; });
 
-/* ! 2 méthodes : laquelle mieux ??? 
-** Demander à Thomas laquelle est la plus correcte
-
-*Close Modal Event and Form
+// close modal event and form
 modalCross.addEventListener("click", function() {
     modalbg.style.display = "none";
-    }));
-
-                OU
-
-*Close Modal Event
-modalCross.addEventListener("click", closeModal);
-
-*Close Modal Form
-function closeModal() {
-    modalbg.style.display = "none";
-}
-
-*/
+});
