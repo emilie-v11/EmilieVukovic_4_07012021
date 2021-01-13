94d30006a13728ab37c106ac6ae2aa45648a2b14
tdimnet <thomas.dimnet@gmail.com>
2021-01-13 14:43:34 +0100

    Session du 13 janvier 2021

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -44,3 +44,36 @@ function closeModal() {
 }
 
 */
+
+
+/**
+ * Session Thomas
+ * 
+ * - 1. Sélectionner le formulaire
+ * - 2. Ajouter un event listener au submit du formulaire (autrement dit, je vais déclencher quand le formulaire va être soumis)
+ * - 3. Sélectionner le noeud (input) prénom
+ * - 4. J'essaye de faire un logger sa valeur au submit du form
+
+ */
+
+// Je sélectionne le noeud .main-form
+const mainForm = document.querySelector('.main-form')
+const firstName = document.querySelector('#first')
+
+
+function checkFirstName(value) {
+  if (value < 2) {
+    console.log("Le formulaire n'est pas bon, la valeur fait moins de 2 caractères")
+  } else {
+    console.log("Tout est ok, cette valeur est correct")
+  }
+}
+
+// Ajouter mon event listener au submit
+mainForm.addEventListener('submit', function(event) {
+  event.preventDefault()
+
+  // Le champ Prénom a un minimum de 2 caractères / n'est pas vide.
+  checkFirstName(firstName.value.length)
+})
+
